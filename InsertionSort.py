fc7b2a2030430ab9bc88e18a588d9d0bdb7e494f
AshinrajM <ashinrajeevank@outlook.com>
2023-12-15 19:18:49 +0530

    Insertion Sort

---
+++ InsertionSort.py
@@ -0,0 +1,15 @@
+def InsertionSort(nums):
+    for i in range(1, len(nums)):
+        temp = nums[i]
+        j = i - 1
+        while temp < nums[j] and j > -1:
+            nums[j + 1] = nums[j]
+            nums[j] = temp
+            j -= 1
+    return nums
+
+
+print(InsertionSort([45, 34, 23, 56, 78, 33, 5]))
+
+# Here comparison starts from 1st index position with previous and then move forward with comparing 
+#continues with previous elements .
---
+++ SelectionSort.py
@@ -0,0 +1,16 @@
+def SelectionSort(nums):
+    n = len(nums)
+    for i in range(n - 1):
+        min_index = i
+        for j in range(i + 1, n):
+            if nums[min_index] > nums[j]:
+                min_index = j
+        nums[i], nums[min_index] = nums[min_index], nums[i]
+    return nums
+
+
+print(SelectionSort([47, 56, 34, 23, 86, 43, 65]))
+
+
+# Here what happends is that using the variable understands at which index position largest value(as per condition)
+# lies . Then value of the variable amd position of elements will be changed
