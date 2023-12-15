bb56b3f753c21cde918b80c7f5d773767597e930
AshinrajM <ashinrajeevank@outlook.com>
2023-12-15 11:14:56 +0530

    bubble sort

---
+++ BubbleSort.py
@@ -0,0 +1,17 @@
+def BubbleSort(nums):
+    n = len(nums)
+
+    for i in range(n):
+        for j in range(n - i - 1):
+            if nums[j] > nums[j + 1]:
+                nums[j], nums[j + 1] = nums[j + 1], nums[j]
+    return nums
+
+
+print(BubbleSort([5, 2, 5, 4, 12, 65, 34]))
+
+
+# In bubble sort this sorting is done by comparing two adjacent elements and if they satisfy the condition
+# they swaps their indices.So In the given after first iteration nums the largest value(Bases on the condition) 
+# will be at the right end of the array.This process continues and array will get sorted
+
